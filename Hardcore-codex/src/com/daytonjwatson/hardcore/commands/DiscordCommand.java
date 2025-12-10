337df9de8c889f7d0992f56a53b4260983439b6c
DaytonJWatson <45436514+DaytonJWatson@users.noreply.github.com>
2025-12-09 18:58:30 -0700

    Add configurable /discord command

---
+++ Hardcore-codex/src/com/daytonjwatson/hardcore/commands/DiscordCommand.java
@@ -0,0 +1,20 @@
+package com.daytonjwatson.hardcore.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import com.daytonjwatson.hardcore.config.ConfigValues;
+import com.daytonjwatson.hardcore.utils.MessageStyler;
+
+public class DiscordCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        var lines = ConfigValues.discordLines();
+        MessageStyler.sendPanel(sender, ConfigValues.translateColor(ConfigValues.discordTitle()),
+                lines.toArray(new String[0]));
+
+        return true;
+    }
+}
